7e5d53124679743382e71a4e5147a53b23e7e189
lukasz <gorcek45@gmail.com>
2021-04-18 20:43:28 +0200

    edit productCard

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,12 +6,7 @@ const Card = ({ image, description }) => {
   try {
     const zoom = React.useRef(mediumZoom());
     return (
-      <div
-        className="mb-8 p-10 pb-5 rounded-lg border border-solid border-gray-200 z-20"
-        style={{
-          boxShadow: '0 10px 28px rgba(0,0,0,.08)'
-        }}
-      >
+      <div className="mb-8 p-10 pb-5 rounded-lg border border-solid border-gray-200 z-20 shadow-inner">
         <Carousel>
           <ImageZoom src={image} zoom={zoom.current} background="#000" />
           <ImageZoom src={image} zoom={zoom.current} background="#000" />
